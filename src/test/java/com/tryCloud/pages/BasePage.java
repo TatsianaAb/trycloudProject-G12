38e57025ce218c17dcde3e69ddfb81922b8b9261
Olha <neolro22.04@gmail.com>
2022-11-27 11:22:10 -0500

    changes

--- src/test/java/com/tryCloud/pages/BasePage.java
+++ src/test/java/com/tryCloud/pages/BasePage.java
@@ -1,12 +1,10 @@
 package com.tryCloud.pages;
 
-import com.tryCloud.utilities.BrowserUtils;
 import com.tryCloud.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
 
 public class BasePage {
 
